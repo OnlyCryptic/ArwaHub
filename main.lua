8467a6961358181aa36d049e8394060193d7622a
OnlyCryptic <bossekasiri2@gmail.com>
2026-02-26 20:04:38 +0000

    Replace OrionLib with Kavo UI Library

--- main.lua
+++ main.lua
@@ -1,15 +1,11 @@
--- 1. استدعاء مكتبة الواجهة
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+-- 1. استدعاء مكتبة Kavo UI
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 
 -- 2. إنشاء النافذة الرئيسية
-local Window = OrionLib:MakeWindow({Name = "Arwa Roger Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "ArwaHub"})
+local Window = Library.CreateLib("Arwa Roger Hub", "Synapse")
 
--- 3. استدعاء ملف خانة اللاعب من حسابك
+-- 3. استدعاء ملف خانة اللاعب من حسابك في جيتهاب
 local loadPlayerTab = loadstring(game:HttpGet("https://raw.githubusercontent.com/OnlyCryptic/ArwaHub/main/PlayerTab.lua"))()
 
--- 4. تشغيل خانة اللاعب وربطها بالنافذة الرئيسية
+-- 4. تشغيل الخانة وربطها بالنافذة
 loadPlayerTab(Window)
-
--- 5. إنهاء إعداد الواجهة لتظهر في اللعبة
-OrionLib:Init()
-
